e05b8e03c2a51c40a47ea6bf06310c68270f0c74
Saad-emb <123195068+Saad-emb@users.noreply.github.com>
2023-10-05 19:56:24 +0100

    Add files via upload

---
+++ src/top.vhd
@@ -0,0 +1,53 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+
+
+entity top is
+port(
+	clk : in std_logic;
+    rst : in std_logic;
+    op : out std_logic_vector(7 downto 0)
+
+);
+
+end entity;
+
+
+architecture behave of top is 
+
+
+component cpu_8bits is
+	port(
+	clk : in std_logic;
+    rst : in std_logic;
+    op : out std_logic_vector(7 downto 0)
+	);
+
+end component;
+
+component divfreq is
+PORT(
+	clk : in std_logic;
+	clk_out : out std_logic
+);
+end component;
+signal clk_out_sig :std_logic;
+begin
+
+
+div_freq :divfreq port map(
+clk=>clk,
+clk_out=>clk_out_sig
+);
+
+
+cpu_obj		: cpu_8bits port map(
+clk => clk_out_sig,
+rst=>not(rst),
+op=>op);
+
+
+end behave;
